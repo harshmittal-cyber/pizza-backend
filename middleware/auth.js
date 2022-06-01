9c81d0f550d17649369034c679cbe48ee5e1bbd9
harshmittal-cyber <you@example.com>
2022-06-01 12:11:24 +0530

    middleware changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,21 +5,32 @@ const Store = require('../models/store');
 const User = require('../models/user');
 
 exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-    const { token } = req.cookies;
-    if (!token) {
-        return next(new ErrorHandler("Please Login to access this resource", 401))
-    }
+    const authHeader = await req.headers.authorization
+    const token = await authHeader.split(" ")[1].toString();
 
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-
     const store = await Store.findById(decodedData.id);
 
     if (!store) {
         return next(new ErrorHandler("Store Not Found", 401))
     }
-
     req.user = store
     next()
+    // const { token } = req.cookies;
+    // if (!token) {
+    //     return next(new ErrorHandler("Please Login to access this resource", 401))
+    // }
+
+    // const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+    // const store = await Store.findById(decodedData.id);
+
+    // if (!store) {
+    //     return next(new ErrorHandler("Store Not Found", 401))
+    // }
+
+    // req.user = store
+    // next()
 })
 
 exports.isUserAuthenticated = catchAsyncErrors(async (req, res, next) => {
